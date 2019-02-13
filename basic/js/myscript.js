bf9eda3d7bcea7af23c26ada10a3dc57ad800a96
JamalAhmed2019 <jamalusb@gmx.at>
2019-02-13 10:22:07 +0100

    Exercise two

---
+++ basic/js/myscript.js
@@ -0,0 +1,46 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//Create the class person
+var Person = /** @class */ (function () {
+    function Person(name, age, jobTitle) {
+        if (jobTitle === void 0) { jobTitle = ""; }
+        this.name = "";
+        this.age = "";
+        this.jobTitle = "";
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    Person.prototype.whoYouAre = function () {
+        return "Hello there, My name is " + this.name + " and I am " + this.age + " years old, and I am a " + this.jobTitle;
+    };
+    return Person;
+}());
+var sentence = new Person("John", 22, "Programmer");
+console.log(sentence.whoYouAre());
+var job = /** @class */ (function (_super) {
+    __extends(job, _super);
+    function job(name, age, jobTitle, salary, jobLocation) {
+        var _this = _super.call(this, name, age, jobTitle) || this;
+        _this.salary = salary;
+        _this.jobLocation = jobLocation;
+        return _this;
+    }
+    job.prototype.whoYouAre = function () {
+        return " " + _super.prototype.whoYouAre.call(this) + " and I get " + this.salary + " every Month and I work in " + this.jobLocation;
+    };
+    return job;
+}(Person));
+var jobsituation = new job("John", 22, "Programmer", 2000, "Vienna");
+console.log(jobsituation.whoYouAre());
--- basic/js/myscript.ts
+++ basic/js/myscript.ts
@@ -5,19 +5,39 @@ class Person {
 	age = "";
 	jobTitle ="";
 
-	constructor(name, age, jobTitle) {
+	constructor(name, age, jobTitle ="") {
 		this.name = name;
 		this.age = age;
 		this.jobTitle = jobTitle;
 	}
 
-	fullName() {
+	whoYouAre() {
 		return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle}`;
 	}
-
-	let sentence = new Person("John", "22", "Programmer");
-	console.log(sentence);
-
-
-
 }
+
+	let sentence = new Person("John", 22,"Programmer");
+	console.log(sentence.whoYouAre());
+
+
+
+class job extends Person{
+	 salary;
+	 jobLocation;
+  
+  constructor(name,age,jobTitle,salary,jobLocation){
+      super(name, age, jobTitle)
+      this.salary=salary;
+      this.jobLocation=jobLocation
+
+
+  }
+  whoYouAre(){
+  	return ` ${super.whoYouAre()} and I get ${this.salary} every Month and I work in ${this.jobLocation}`
+  }
+}
+var jobsituation= new job("John", 22,"Programmer", 2000,"Vienna")
+console.log(jobsituation.whoYouAre());
+
+	
+
